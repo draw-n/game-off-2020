a5b6f4459b0f8e79540b48988de7e6d1cc1e8ea4
draw-n <69174251+draw-n@users.noreply.github.com>
2020-11-01 23:05:10 -0500

    Updates

--- Godot/Player/SpiritPlayer.gd
+++ Godot/Player/SpiritPlayer.gd
@@ -1,30 +1,32 @@
 extends KinematicBody2D
 
+signal leave
+
+const GRUNT = preload("res://Player/GruntPlayer.tscn")
+
 export var ACCELERATION = 10000
 export var MAX_SPEED = 500
 export var FRICTION = .25
 
-enum {
-	MOVE,
-	CONTROL
-}
-
-var state = MOVE
 var velocity = Vector2.ZERO
 
+onready var sprite = $Sprite
+onready var camera2D = $Camera2D
 onready var animationPlayer = $AnimationPlayer
+onready var position2D = $Position2D
 
 func _ready():
-	pass
+	camera2D.current = true
 
 func _physics_process(delta):
-	match state:
-		MOVE: 
-			move_state(delta)
-		CONTROL:
-			pass
+	move_state(delta)
+	control()
 
 func move_state(delta):
+	if velocity.x > 0:
+		sprite.flip_h = false
+	elif velocity.x < 0:
+		sprite.flip_h = true
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -35,8 +37,6 @@ func move_state(delta):
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	velocity = move_and_slide(velocity)
-<<<<<<< Updated upstream
-=======
 
 func control():
 	if Input.is_action_just_pressed("ui_accept"):
@@ -49,4 +49,3 @@ func _on_ControlReceive_area_entered(area):
 
 func _on_SpiritPlayer_leave():
 	queue_free()
->>>>>>> Stashed changes
